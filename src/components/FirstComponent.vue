85a336e31dda24952bbf8b356d70166431b2e098
Lara <lararefojos@gmail.com>
2021-10-21 15:58:33 +0200

    feature(FirstComponent): v-for example

--- src/components/FirstComponent.vue
+++ src/components/FirstComponent.vue
@@ -4,6 +4,11 @@
     <p v-if="condition">This message will not be displayed</p>
     <p v-else-if="list.length !== 0">This message will be displayed</p>
     <p v-else>Not show</p>
+    <ul>
+      <li v-for="(item, i) in elements" :key="i">
+        ID: {{ item.id }} - {{ item.name }}
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -13,6 +18,11 @@ export default {
   data: () => ({
     condition: false,
     list: [0, 2, 3, 4, 5],
+    elements: [
+      { name: "Elemento 1", id: 1 },
+      { name: "Elemento 2", id: 2 },
+      { name: "Elemento 3", id: 3 },
+    ],
   }),
 };
 </script>
