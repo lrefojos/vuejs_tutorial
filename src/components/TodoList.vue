4f2d2c8c7b269a53afa77a98f9b10874e849c650
Lara <lararefojos@gmail.com>
2021-10-22 13:58:58 +0200

    feature(*): create dummy todo list with a form
    Display the list of tasks
    Remove home component

---
+++ src/components/TodoList.vue
@@ -0,0 +1,49 @@
+<template>
+  <form class="task" id="app" @submit.prevent="createTask">
+    <label class="label" for="task">Nueva tarea: </label>
+    <input type="text" v-model="newTask" id="task" />
+    <input type="submit" value="Crear tarea" />
+    <ul>
+      <li
+        class="task__list"
+        :class="{ 'task__list-completed': task.completed }"
+        v-for="(task, i) in tasks"
+        :key="'task' + i"
+      >
+        {{ task.text }}
+      </li>
+    </ul>
+  </form>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    newTask: "",
+    tasks: [],
+  }),
+  methods: {
+    createTask() {
+      let task = {
+        text: this.newTask,
+        completed: false,
+      };
+      this.tasks.push(task);
+      this.newTask = "";
+      console.log(this.tasks);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.task {
+  &__list {
+    list-style-type: none;
+    &-completed {
+      text-decoration: line-through;
+      color: grey;
+    }
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,27 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <TodoList />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import TodoList from "@/components/TodoList.vue";
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    TodoList,
   },
 };
 </script>
+
+<style lang="scss" scoped>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
