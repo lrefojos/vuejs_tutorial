7ac2a27899e4e092284e337dbdca1ac13c674f90
Lara <lararefojos@gmail.com>
2021-10-22 16:18:27 +0200

    style(TodoList): refactor and more sytles

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,20 +1,22 @@
 <template>
-  <form class="task" id="app" @submit.prevent="createTask">
-    <label class="label" for="task">Nueva tarea: </label>
-    <input type="text" v-model="newTask" id="task" />
-    <input type="submit" value="Crear tarea" />
-    <ul>
-      <li
-        class="task__list"
-        :class="{ 'task__list-completed': task.completed }"
-        v-for="(task, i) in tasks"
-        :key="'task' + i"
-        @click="completedTask(task.text)"
-      >
-        {{ task.text }}
-      </li>
-    </ul>
-  </form>
+  <div class="task">
+    <form class="task__form" id="app" @submit.prevent="createTask">
+      <label class="task__label" for="task">Nueva tarea: </label>
+      <input class="task__input" type="text" v-model="newTask" id="task" />
+      <input class="task__button" type="submit" value="Crear tarea" />
+      <ul class="task__list">
+        <li
+          class="task__element"
+          :class="{ 'task__element-completed': task.completed }"
+          v-for="(task, i) in tasks"
+          :key="'task' + i"
+          @click="completedTask(task.text)"
+        >
+          {{ task.text }}
+        </li>
+      </ul>
+    </form>
+  </div>
 </template>
 
 <script>
@@ -43,8 +45,39 @@ export default {
 
 <style lang="scss" scoped>
 .task {
+  width: 800px;
+  max-width: 100%;
+  margin: 0px auto;
+  &__form {
+    background: white;
+    border-radius: 12px;
+    padding: 30px;
+    box-shadow: 0px 10px 22px -1px rgba(0, 0, 0, 0.25);
+  }
+  &__label {
+    display: block;
+    margin-bottom: 10px;
+  }
+  &__input {
+    height: 35px;
+  }
+  &__button {
+    margin-left: 20px;
+    height: 35px;
+    border: none;
+    border-radius: 5px;
+    box-shadow: 0 1px 4px rgba(0, 0, 0.2);
+    background-color: #2ecc71;
+    color: #ecf0f1;
+    cursor: pointer;
+  }
   &__list {
+    margin-top: 40px;
     list-style-type: none;
+  }
+  &__element {
+    cursor: pointer;
+    margin: 10px 0;
     &-completed {
       text-decoration: line-through;
       color: grey;
