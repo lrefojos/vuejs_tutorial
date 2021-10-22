6c987b1f0eb7f7aaad5d23f464a1351d036a5f14
Lara <lararefojos@gmail.com>
2021-10-22 09:52:21 +0200

    feature(SecondComponent): onClick event

--- src/components/SecondComponent.vue
+++ src/components/SecondComponent.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="content">
     {{ calculate() }}
+    <div class="content__div">
+      <button class="content__button" @click="increment()">Increment</button>
+      <span> Current value {{ value }}</span>
+    </div>
   </div>
 </template>
 
@@ -11,12 +15,24 @@ export default {
   }),
   methods: {
     calculate() {
-      let aux = this.value * 2;
-      console.log(aux);
       return this.value * 2;
+    },
+    increment() {
+      this.value++;
     },
   },
 };
 </script>
 
-<style lang="scss" scoped></style>;
+<style lang="scss" scoped>
+.content {
+  &__div {
+    padding: 20px;
+  }
+  &__button {
+    height: 100px;
+    width: 100px;
+    background-color: lightcoral;
+  }
+}
+</style>;
