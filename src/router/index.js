3ebf3025ea2eaae43f2a06eb527c0ef05349e8e8
Lara <lararefojos@gmail.com>
2021-10-25 09:21:07 +0200

    wip

--- src/router/index.js
+++ src/router/index.js
@@ -19,6 +19,11 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
+  {
+    path: "/examples",
+    name: "Examples",
+    component: () => import("../views/Examples.vue")
+  }
 ];
 
 const router = new VueRouter({
---
+++ src/views/Examples.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="examples">
+    <h1>Examples</h1>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang="scss" scoped>
+</style>
